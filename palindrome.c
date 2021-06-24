cad618ab15ef64f5f0c0b718da19e1016578ecbb
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-06-25 03:10:23 +0530

    palindrome

---
+++ palindrome.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+ int num,r,sum=0,temp;
+ printf("enter the no:");
+ scanf("%d",&num);
+ temp=num;
+ while (num>0)
+ {
+
+     r=num%10;
+     sum=(sum*10)+r;
+     num=num/10;
+ }
+ if(sum==temp)
+ {
+     printf(" no is palindrome");
+ }
+ else
+ {
+     printf("no is not a palindrome");
+ }
+ return 0;
+}
